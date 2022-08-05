078b063aafc39d053a39e7a406d75168c761bf20
Ryan Etedali <ryanetedali@gmail.com>
2022-08-04 23:22:10 -0500

    compeleted Navbar and EntryForm stylings

--- src/Components/AddEntryForm/AddEntryForm.jsx
+++ src/Components/AddEntryForm/AddEntryForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './AddEntryForm.css'
 
 const AddEntryForm = (props) => {
 
@@ -15,17 +16,17 @@ const AddEntryForm = (props) => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" name="Name" value={name} onChange={(event) => setName(event.target.value)}/>
-                </label><br />
-                <label>
-                    Post:
-                    <textarea rows={8} type="text" name="Post" onChange={(event) => setPost(event.target.value)}/>
-                </label><br />
-                <input type="submit" value="Add" />
+        <div className='entry-box'>
+            <form onSubmit={handleSubmit} className='form-grid'>
+                <div>
+                    <label className='name'>
+                    <h5>  Name:</h5><textarea rows={1} type="text" name="Name" value={name} onChange={(event) => setName(event.target.value)}/>
+                    </label><br />
+                    <label className='post'>
+                            <h5>  Post:</h5><textarea rows={3} type="text" name="Post" value={post} onChange={(event) => setPost(event.target.value)}/>
+                    </label><br />
+                    <input type="submit" value="Add" className='button' />
+                </div>
             </form>
         </div>
 
